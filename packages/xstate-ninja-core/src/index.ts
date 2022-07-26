d1971e32509b163f7515a52fef9508cd8656ac2b
Richard Laffers <rlaffers@gmail.com>
2022-07-27 00:20:39 +0200

    build(xstate-ninja-core): configure vitejs for xstate-ninja-core

---
+++ packages/xstate-ninja-core/src/index.ts
@@ -0,0 +1,3 @@
+type Foo = string[]
+
+export const foo: Foo = ['it works!']
---
+++ packages/xstate-ninja-core/vite.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('vite').UserConfig} */
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'XStateNinja',
+      fileName: 'xstate-ninja',
+    }
+  }
+})
