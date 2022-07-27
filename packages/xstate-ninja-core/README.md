6ad71132d79e260dc67f5d6c78855786b2386c50
Richard Laffers <rlaffers@gmail.com>
2022-07-27 02:27:58 +0200

    feat(core): reexport xstate's interpret and spawn, add TS types

---
+++ packages/xstate-ninja-core/README.md
@@ -0,0 +1,13 @@
+# xstate-ninja-core
+
+This is the core library. It exports Typescript types and hooks for vanilla JS.
+
+```javascript
+import type { WindowWithXStateNinja } from 'xstate-ninja'
+
+declare let window: WindowWithXStateNinja
+```
+
+## TODO
+
+- add NodeJS support
--- packages/xstate-ninja-core/src/index.ts
+++ packages/xstate-ninja-core/src/index.ts
@@ -1,3 +1,42 @@
-type Foo = string[]
+import { interpret as xstateInterpret, spawn as xstateSpawn } from 'xstate'
+import type {
+  InterpreterOptions,
+  ActorRef,
+  Spawnable,
+  AnyStateMachine,
+  AnyInterpreter,
+  AnyActorRef,
+} from 'xstate'
 
-export const foo: Foo = ['it works!']
+// SpawnOptions are not exported from xstate, so here's a copy
+interface SpawnOptions {
+  name?: string
+  autoForward?: boolean
+  sync?: boolean
+}
+
+const extensionAPI = window.__xstate_ninja__
+
+export function interpret(
+  machine: AnyStateMachine,
+  options: Partial<InterpreterOptions>,
+) {
+  const service = xstateInterpret(machine, options)
+  extensionAPI?.register(service)
+  return service
+}
+
+export function spawn(
+  entity: Spawnable,
+  nameOrOptions?: string | SpawnOptions,
+): ActorRef<any> {
+  return xstateSpawn(entity, nameOrOptions)
+}
+
+export function register(actor: AnyInterpreter | AnyActorRef) {
+  return extensionAPI?.register(actor)
+}
+
+export function unregister(actor: AnyInterpreter | AnyActorRef) {
+  return extensionAPI?.unregister(actor)
+}
