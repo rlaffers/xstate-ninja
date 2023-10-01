613fbab1ac1880ab9ca289d4d5d150a5c5e73934
Richard Laffers <rlaffers@gmail.com>
2023-10-01 09:03:29 +0200

    fix(example-vue): remove VueMacros

--- example-vue/vite.config.ts
+++ example-vue/vite.config.ts
@@ -6,7 +6,7 @@ import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 // import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import VueMacros from 'unplugin-vue-macros/vite'
+// import VueMacros from 'unplugin-vue-macros/vite'
 
 export default defineConfig({
   resolve: {
@@ -15,16 +15,22 @@ export default defineConfig({
     },
   },
   plugins: [
-    VueMacros({
-      defineOptions: false,
-      defineModels: false,
-      plugins: {
-        vue: Vue({
-          script: {
-            propsDestructure: true,
-            defineModel: true,
-          },
-        }),
+    // VueMacros({
+    //   defineOptions: false,
+    //   defineModels: false,
+    //   plugins: {
+    //     vue: Vue({
+    //       script: {
+    //         propsDestructure: true,
+    //         defineModel: true,
+    //       },
+    //     }),
+    //   },
+    // }),
+    Vue({
+      script: {
+        propsDestructure: true,
+        defineModel: true,
       },
     }),
 
